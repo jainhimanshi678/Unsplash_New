68254881a03e99425e9041466b9c2daf0ceafffb
Himanshi Jain <Himanshi.jain@kratin.co.in>
2022-03-27 17:58:36 +0530

    Set home fragment as default fragment Work on homefragment layout

--- app/src/main/java/com/sk/unsplash/UI/Fragment/HomeFragment.kt
+++ app/src/main/java/com/sk/unsplash/UI/Fragment/HomeFragment.kt
@@ -1,13 +1,19 @@
 package com.sk.unsplash.UI.Fragment
 
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import com.sk.unsplash.R
 import com.sk.unsplash.base.BaseFragment
 import com.sk.unsplash.databinding.FragmentHomeBinding
+import androidx.appcompat.app.AppCompatActivity
+
+
+
 
 class HomeFragment : BaseFragment() {
 
@@ -26,6 +32,11 @@ class HomeFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding= FragmentHomeBinding.bind(view)
         setToolBar()
+    }
+
+    private fun setToolBar(){
+        //(activity as AppCompatActivity?)?.setSupportActionBar(binding.toolbar)
     }
 }
--- app/src/main/java/com/sk/unsplash/interfaces/IMainActivity.kt
+++ app/src/main/java/com/sk/unsplash/interfaces/IMainActivity.kt
@@ -1,5 +1,5 @@
 package com.sk.unsplash.interfaces
 
 interface IMainActivity {
-    fun setToolBar(isHomeFragment:Boolean,shouldShowBackButton:Boolean,name:String)
+
 }
